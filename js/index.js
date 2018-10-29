d61e5e53d7c34b174f533b7cb0ead333768a8e1a
Thomas Green <Thomas.Green@getbellhops.com>
2018-10-29 19:51:44 -0400

    updated some divs

--- js/index.js
+++ js/index.js
@@ -1,16 +1,24 @@
 <script type="text/javascript">
+// {
 
-    var name = prompt('What is your name?');
-  
+// var name = prompt("Hi there! What's your name?");
+// var output = document.querySelector("#greeting");
+// output.textContent = "Thanks for visiting, " + name + ".";
+//   var name = prompt('What is your name?');
+// }
+
+{
     var checkName = function() {
       if (name === "" ) {
         name = prompt('What is your name please?')}
   
         checkName(); // repeats until a name exists
-      }
+      
       
     }
+
   
+
     checkName(); // kicks off the name-checking the first time
   
     alert('Hello ' + name);
