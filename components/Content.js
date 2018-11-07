8e5998001a8f9bc0f1fafbd48267276dfcccf4c4
Thomas Green <Thomas.Green@getbellhops.com>
2018-11-07 16:01:40 -0500

    added some styles

--- components/Content.js
+++ components/Content.js
@@ -3,11 +3,16 @@ export default
 
 `<div class="container content-area">
 
+
+
 <ul >
-    
+   
+
+    <li><marquee>Welcome to my site!  I hope you enjoy your stay 😎</marquee> </li>
     <li>Thomas Green</li>
     <li>Chattanooga, TN</li>
     
+  
     
 </ul>
 
--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,11 @@ export default
   
 
 
-<h1>Thomas Green Savvy Coders Portfolio</h1>
+
+<img src="https://ourroaminghearts.com/wp-content/uploads/2018/02/Walnut-Street-Bridge-Free-thing-to-do-in-Chattanooga.png" alt="bridge" id="bridge" > 
+
+
+
 
 <img src="https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/26239830_10154902451422101_8064666191664269545_n.jpg?_nc_cat=111&oh=963dc4a104004e80179087d7c545e1fa&oe=5C5B5641" alt="profile picture" id="profile" >    
 
